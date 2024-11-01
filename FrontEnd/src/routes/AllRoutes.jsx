c251b07bd2e1a6964582ca19491ce352735dcacd
PWM-Alex <VictorAlex1233@hotmail.com>
2024-11-01 13:54:52 -0600

    Catalogo para los clientes
    trabajo en proceso

---
+++ FrontEnd/src/routes/AllRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from '../access-control/components/Login'
+import RecoveryPassword from '../access-control/components/RecoveryPassword'
+import Register from '../access-control/components/Register'
+import Catalogue from '../Catalogue-client/catalogue'
+
+function AllRoutes() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/recovery' element={<RecoveryPassword />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/catalogue' element={<Catalogue/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AllRoutes
