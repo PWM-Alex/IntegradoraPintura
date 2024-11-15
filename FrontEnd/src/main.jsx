fa7608f1e902889ab0f237bb668eb23441283dfb
PWM-Alex <VictorAlex1233@hotmail.com>
2024-11-15 10:34:47 -0600

    Cambio de mi rama correciones

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import 'primereact/resources/themes/lara-light-blue/theme.css'; 
+import 'primeicons/primeicons.css'; 
+import '/node_modules/primeflex/primeflex.css'; 
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
